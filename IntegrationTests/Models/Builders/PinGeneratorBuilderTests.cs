5dc80143005be3b17752db670f8da2989e0a109a
Anton Tolken <anton.tolken@icloud.com>
2024-12-17 13:24:35 +1300

    add more pin generator builder tests

--- IntegrationTests/Models/Builders/PinGeneratorBuilderTests.cs
+++ IntegrationTests/Models/Builders/PinGeneratorBuilderTests.cs
@@ -1,4 +1,5 @@
 using PinGenerator.Models.Builders;
+using PinGenerator.Models.Digits;
 
 namespace IntegrationTests.Models.Builders;
 
@@ -9,9 +10,29 @@ public class PinGeneratorBuilderTests
     {
         PinGeneratorBuilder builder = new();
         PinGenerator.Models.Generators.PinGenerator pinGenerator = builder.Build();
-        string pin = pinGenerator.CreatePin(4);
 
         Assert.That(pinGenerator.GetType(), Is.EqualTo(typeof(PinGenerator.Models.Generators.PinGenerator)));
+        
+        UsePinBuilderPinGenerator(pinGenerator);
+    }
+
+    [Test]
+    public void PinGeneratorBuilder_ShouldBuild_WithSpecificChoices()
+    {
+        PinGeneratorBuilder builder = new(new NumericDigitProvider());
+        builder.AddNoIncrementalDigitsRule();
+        builder.AddNoConsecutiveRepeatDigitsRule();
+        PinGenerator.Models.Generators.PinGenerator pinGenerator = builder.Build();
+
+        Assert.That(pinGenerator.GetType(), Is.EqualTo(typeof(PinGenerator.Models.Generators.PinGenerator)));
+        
+        UsePinBuilderPinGenerator(pinGenerator);
+    }
+
+    private static void UsePinBuilderPinGenerator(PinGenerator.Models.Generators.PinGenerator pinGenerator)
+    {
+        string pin = pinGenerator.CreatePin(4);
+
         Assert.That(pin, Is.Not.Null);
         Assert.That(pin.Length, Is.EqualTo(4));
     }
