3a6e61a67d8a7998c3311b7a0b5927038c6bc838
Anton Tolken <anton.tolken@icloud.com>
2024-12-17 13:30:52 +1300

    add basic program output for the non-TDD crowd

--- PinGenerator/Program.cs
+++ PinGenerator/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // todo-at: add editorconfig?
-Console.WriteLine("Hello, World!");
 
+using PinGenerator.Models.Builders;
+using PinGenerator.Models.Generators;
+
+const int pinLength = 4;
+
+// The code is mainly exercised in the unit and integration test projects.
+PinGeneratorBuilder builder = new();
+PinGenerator.Models.Generators.PinGenerator pinGenerator = builder.Build();
+PinsGenerator pinsGenerator = new(pinGenerator, pinLength);
+HashSet<string> pins = pinsGenerator.CreateUniquePins(100);
+foreach (string pin in pins)
+{
+    Console.Write($"{pin} ");
+}
